e11558306a56da1ffdf732f5c6632ce1f9689ca3
waitfortea <2997933928@qq.com>
2024-03-26 14:26:24 +0800

    clear

--- hook.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append(r'D:\environment\python')
-sys.path.append(r'D:\environment\python\Lib')
-sys.path.append(r'D:\environment\python\Lib\site-packages')
-
-print(sys.path)
-
