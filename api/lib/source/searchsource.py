e3e25557223deb58678259bb3b8144e66f5df496
waitfortea <2997933928@qq.com>
2025-12-23 22:00:12 +0800

    周二 2025/12/23 22:00:12.08

--- api/lib/source/searchsource.py
+++ api/lib/source/searchsource.py
@@ -56,21 +56,29 @@ def bthome_batch_searchplugin(keyword: object, page_range: object, *args: object
 
 @BtHomeUtils.register_sourceplugin('bthome_multi_search')
 def bthome_multi_searchplugin(keyword: object, page_range: object, *args: object, **kwargs: object) -> List[Any]:
+    url_list = []
+    for page in range(1, page_range + 1):
+        url = re.sub("forum-17.*(?<=\.)",f"forum-17-{page}.",config['source']['bthome_multi']['domain'])
+        url_list.append(url)
 
-    url = config['source']['bthome_multi']['domain']
 
-    htmlText = RequestUitls.get_html(name='dp_one_tab', type='get', url=url)
+    html_list = RequestUitls.get_html(name='dp_multi_tab', type='get', url=url_list)
 
-    doc = ElementUtils.parse_html(htmlText)
+    torrentpage_list = []
+    for htmltext in html_list:
+        doc = ElementUtils.parse_html(htmltext)
 
-    element_List = doc.xpath('//div[@class="media-body"]//a[contains(@href,"thread")]')
+        element_List = doc.xpath('//div[@class="media-body"]//a[contains(@href,"thread") and contains(@class,"text")]')
 
-    if not element_List:
-        EventUtils.run('infolog', logdata='搜索结果为空')
+        if not element_List:
+            EventUtils.run('infolog', logdata='搜索结果为空')
 
-    result_list = [TorrentPage(url=config['source']['bthome']['domain'] + "/" + element.attrib['href']
-                               , title=ElementUtils.get_text(element)) for element in element_List]
-    return result_list
+        result_list = [TorrentPage(url=config['source']['bthome']['domain'] + "/" + element.attrib['href']
+                                   , title=ElementUtils.get_text(element)) for element in element_List]
+        torrentpage_list.extend(result_list)
+
+    return torrentpage_list
+
 
 @BtHomeUtils.register_sourceplugin('comicgarden_search')
 def commicgarden_searchplugin(keyword, page_range, *args, **kwargs):
